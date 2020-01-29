02f8bc7843f9cc82c04cec2ad434fe3b2ae1b2f0
syaakovi <syaakovi@paloaltonetworks.com>
2020-01-29 15:45:34 +0200

    temp add constants

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -31,12 +31,12 @@ if [ -z "${ARTIFACT_BUILD_NUM}" ]
 fi
 
 export SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/ | head -n 1)
-# TEMP_LINK=${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
+TEMP_LINK=${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
 # SERVER_DOWNLOAD_LINK=${TEMP_LINK%$'\r'}
 
 echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
 echo "curl to server installer: $SERVER_DOWNLOAD_LINK"
 
-# curl ${SERVER_DOWNLOAD_LINK} --output demistoserver.sh
+# curl ${TEMP_LINK%$'\r'} --output demistoserver.sh
 
 echo "Done!"
